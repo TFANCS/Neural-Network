9052abbc4fee430af969e639db6187d76012d0b7
TFANCS <tsnf.hc.p.mlp2@gmail.com>
2020-02-09 15:47:30 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,45 +9,44 @@ namespace NeuralNetwork1 {
 
         static void Main(string[] args) {
 
-            Neuron neuronIn1 = new Neuron(1);
-            Neuron neuronIn2 = new Neuron(1);
-            Neuron neuronMid1 = new Neuron(1);
-            Neuron neuronMid2 = new Neuron(1);
-            Neuron neuronMid3 = new Neuron(1);
-            Neuron neuronOut = new Neuron(0);
-            neuronMid1.Connect(new Neuron[2] { neuronIn1, neuronIn2 });
-            neuronMid2.Connect(new Neuron[2] { neuronIn1, neuronIn2 });
-            neuronMid3.Connect(new Neuron[2] { neuronIn1, neuronIn2 });
-            neuronOut.Connect(new Neuron[3] { neuronMid1, neuronMid2, neuronMid3 });
+            List<Layer> layers = new List<Layer>();
+
+
+            layers.Add(new Affine(5,3));
+            layers.Add(new Sigmoid());
+            layers.Add(new Affine(1,5));
+            layers.Add(new Step());
+            layers.Add(new Identity());
 
             List<TrainingSet> trainingSets = new List<TrainingSet>();
 
-            for (int i = 0; i < 800; i++) {
-                trainingSets.Add(new TrainingSet(new double[2] { 0, 0 }, new double[1] { 0}));
-                trainingSets.Add(new TrainingSet(new double[2] { 0, 1 }, new double[1] { 1}));
-                trainingSets.Add(new TrainingSet(new double[2] { 1, 0 }, new double[1] { 1}));
-                trainingSets.Add(new TrainingSet(new double[2] { 1, 1 }, new double[1] { 0}));
+            for (int i = 0; i < 1000; i++) {
+                trainingSets.Add(new TrainingSet(new double[3] { 0, 0, 0 }, new double[1] { 0 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 0, 0, 1 }, new double[1] { 1 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 0, 1, 0 }, new double[1] { 1 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 0, 1, 1 }, new double[1] { 0 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 1, 0, 0 }, new double[1] { 1 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 1, 0, 1 }, new double[1] { 0 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 1, 1, 0 }, new double[1] { 0 }));
+                trainingSets.Add(new TrainingSet(new double[3] { 1, 1, 1 }, new double[1] { 1 }));
             }
 
 
-
-
+            
             for (int i = 0; i < trainingSets.Count(); i++) {
-                neuronIn1.SetValue(trainingSets[i].Features[0]);
-                neuronIn2.SetValue(trainingSets[i].Features[1]);
-                neuronMid1.ForwardPropagation();
-                neuronMid2.ForwardPropagation();
-                neuronMid3.ForwardPropagation();
-                neuronOut.ForwardPropagation();
-                neuronOut.CalcError(trainingSets[i].Target[0]);
-                neuronOut.BackPropagation();
-                double result = neuronOut.GetValue();
-                Console.Write("Expectd : "+ trainingSets[i].Target[0] + "   result: " + result + "   ");
-                if (Math.Abs(result - trainingSets[i].Target[0]) < 0.1) Console.WriteLine("v");
+                double[] data = (double[])trainingSets[i].Features.Clone();
+                for (int j = 0; j < layers.Count(); j++) {
+                    data = layers[j].ForwardPropagation(data);
+                }
+                double[] error = (double[]) trainingSets[i].Target.Clone();
+                for (int j = layers.Count()-1; j >= 0; j--) {
+                    error = layers[j].BackPropagation(error);
+                }
+                Console.Write(String.Format("Expected : {0}   result: {1}   ", trainingSets[i].Target[0], data[0]));
+                if (Math.Abs(data[0] - trainingSets[i].Target[0]) < 0.1) Console.WriteLine("v");
                 else Console.WriteLine("X");
             }
-
-
+            
 
         }
     }
@@ -66,171 +65,186 @@ namespace NeuralNetwork1 {
 
 
 
+    abstract class Layer {
+        abstract public double[] ForwardPropagation(double[] input);
+        abstract public double[] BackPropagation(double[] input);
+    }
 
-    class Neuron {
+
+
+    class Affine : Layer{
         static Random random = new Random();
 
-        private double[] weight = new double[3];
-        private double bias = 0;
-        private readonly double learningRate = 0.5;
-        private double value = 0;    //sum of weighted imputs and bias
-        private double error = 0;    //difference between target and value
+        private double[] value;
+        private double[,] weight;
+        private double[] bias;
 
-        private readonly int actFunc;  //0->step  1->sigmoid  2->ReLU
-
-        private Neuron[] input;
-
-        public Neuron(int actFunc) {
-            this.actFunc = actFunc;
-            bias = random.NextDouble();
-            for (int i = 0; i < weight.Length; i++) {
-                weight[i] = random.NextDouble();
+        public Affine(int nodes, int inputNodes){
+            weight = new double[nodes,inputNodes];
+            bias = new double[nodes];
+            for (int i = 0; i < weight.GetLength(0); i++) {
+                for (int j = 0; j < weight.GetLength(1); j++) {
+                    weight[i,j] = (Math.Sqrt(-2.0 * Math.Log(random.NextDouble())) * Math.Sin(2.0 * Math.PI * random.NextDouble())) * (1.0 / Math.Sqrt(weight.GetLength(1)));
+                }
             }
         }
 
-        public void Connect(in Neuron[] input) {
-            this.input = input;
-            return;
-        }
-
-        public void SetValue(double value) {
-            this.value = value;
-        }
-
-        public double GetValue() {
-            return value;
-        }
-
-        public void SetWeight(double weight, int index) {
-            this.weight[index] = weight;
-            return;
-        }
-
-
-
-        public void ForwardPropagation() {
-            int length = input.Length;
-
-            double sum = 0;
-            for (int i = 0; i < length; i++) {
-                sum += input[i].GetValue() * weight[i];
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = new double[weight.GetLength(0)];
+            for (int i = 0; i < weight.GetLength(0); i++) {
+                double sum = 0;
+                for (int j = 0; j < weight.GetLength(1); j++) {
+                    sum += (input[j] * weight[i,j]);
+                }
+                output[i] = sum + bias[i];
             }
-            sum += bias;
-            double result = 0;
-            switch (actFunc) {
-                case 0:
-                    result = Step(sum);
-                    break;
-                case 1:
-                    result = Sigmoid(sum);
-                    break;
-                case 2:
-                    result = ReLU(sum);
-                    break;
-            }
-            value = result;
-            
-            return;
+            value = (double[])input.Clone();
+            return output;
         }
 
-
-
-        public void BackPropagation() {
-            int length = input.Length;
-
-            for (int i = 0; i < length; i++) {
-                input[i].UpdateError(error, weight[i]);
-                input[i].UpdateWeight();
+        override public double[] BackPropagation(double[] input) {
+            double[] output = new double[weight.GetLength(1)];
+            for (int j = 0; j < weight.GetLength(1); j++) {
+                for (int i = 0; i < weight.GetLength(0); i++) {
+                    output[j] += (input[i] * weight[i,j]);
+                }
             }
 
-            return;
-        }
-
-
-
-        public void CalcError(double target) {
-            error = (target - value);
-            UpdateWeight();
-        }
-
-
-        public void UpdateError(double errorBack, double weightBack) {
-            int length = input.Length;
-
-            switch (actFunc) {
-                case 0:
-                    error = (errorBack * weightBack);
-                    break;
-                case 1:
-                    error = (errorBack * weightBack * SigmoidDeriv(value));
-                    break;
-                case 2:
-                    error = (errorBack * weightBack);
-                    break;
+            for (int j = 0; j < weight.GetLength(1); j++) {
+                for (int i = 0; i < weight.GetLength(0); i++) {
+                    weight[i, j] -= input[i] * value[j] * 0.5;
+                }
             }
-            return;
-        }
-
-
-
-        public void UpdateWeight() {
-            int length = input.Length;
-
-            switch (actFunc) {
-                case 0:
-                    for (int i = 0; i < length; i++) {
-                        weight[i] = weight[i] + (learningRate * error * input[i].GetValue());
-                    }
-                    bias = bias + (learningRate * error);
-                    break;
-                case 1:
-                    for (int i = 0; i < length; i++) {
-                        weight[i] = weight[i] + (learningRate * error * SigmoidDeriv(value) * input[i].GetValue());
-                    }
-                    bias = bias + (learningRate * error * SigmoidDeriv(value));
-                    break;
-                case 2:
-                    for (int i = 0; i < length; i++) {
-                        weight[i] = weight[i] + (learningRate * error * input[i].GetValue());
-                    }
-                    bias = bias + (learningRate * error);
-                    break;
+            for (int i = 0; i < weight.GetLength(0); i++) {
+                bias[i] -= input[i] * 0.5;
             }
+
+            return output;
         }
-
-
-
-        private double ReLU(double x) {
-            if (x >= 0) {
-                return x;
-            }else {
-                return 0;
-            }
-        }
-
-        private double Sigmoid(double x) {
-            return 1 / (1+Math.Exp(-x));
-        }
-
-
-        private double SigmoidDeriv(double y) {
-            return y*(1-y);
-        }
-
-
-        private double Step(double x) {
-            if (x >= 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-
-
     }
 
 
 
 
-}
+    class Sigmoid : Layer {
+        private double[] value;
 
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = 1.0 / (1.0 + Math.Exp(-input[i]));
+            }
+            value = (double[])output.Clone();
+            return output;
+        }
+
+        override public double[] BackPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = input[i] * ((1.0 - value[i]) * value[i]);
+            }
+            return output;
+        }
+    }
+
+
+
+
+    class ReLU : Layer {
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] >= 0) {
+                    output[i] = input[i];
+                } else {
+                    output[i] = 0;
+                }
+            }
+            return output;
+        }
+
+        override public double[] BackPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] >= 0) {
+                    output[i] = 1;
+                } else {
+                    output[i] = 0;
+                }
+            }
+            return output;
+        }
+    }
+
+
+
+
+
+    class Step : Layer {
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                if (input[i] >= 0) {
+                    output[i] = 1;
+                } else {
+                    output[i] = 0;
+                }
+            }
+            return output;
+        }
+
+        override public double[] BackPropagation(double[] input) {
+            double[] output = (double[])input.Clone();
+            return output;
+        }
+    }
+
+
+
+
+
+    class Softmax : Layer {
+        private double[] value;
+
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            double sum = input.Sum();
+            double max = input.Max();
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = Math.Exp(input[i] - max)/Math.Exp(sum - max);
+            }
+            value = (double[])input.Clone();
+            return output;
+        }
+
+        override public double[] BackPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = value[i] - input[i];
+            }
+            return output;
+        }
+    }
+
+
+
+    class Identity : Layer {
+        private double[] value;
+
+        override public double[] ForwardPropagation(double[] input) {
+            double[] output = (double[])input.Clone();
+            value = (double[])input.Clone();
+            return output;
+        }
+
+        override public double[] BackPropagation(double[] input) {
+            double[] output = new double[input.Length];
+            for (int i = 0; i < input.Length; i++) {
+                output[i] = value[i] - input[i];
+            }
+            return output;
+        }
+    }
+
+
+}
